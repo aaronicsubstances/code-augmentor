b65167fd108edc1f08ee9a795d5f7dbc4d808f4d
Aaron Baffour-Awuah <aaronbaffourawuah@gmail.com>
2020-06-18 08:19:59 +0000

    Updated OS shell scripts to be extensible with arbitrary OS commands.

--- code-augmentor-core/src/main/resources/com/aaronicsubstances/code/augmentor/core/tasks/unix-copy-bash-file.sh
+++ code-augmentor-core/src/main/resources/com/aaronicsubstances/code/augmentor/core/tasks/unix-copy-bash-file.sh
@@ -3,6 +3,20 @@
 if [ "$1" == "-f" ]; then confirmed=1; fi
 DIRNAME=`dirname $0`
 if [ -z "$DIRNAME" ]; then DIRNAME=.; fi
+
+CMDNAME=`basename $0`
+
+if [ "$1" == "--rev" ]; then
+    reverseFileNames=1
+    shift
+fi
+
+arbitraryCommand="$@"
+if [ "$arbitraryCommand" == "-f" ]; then
+    set confirmed=1
+    arbitraryCommand=
+fi
+
 while IFS= read -r line
 do
     if [ -n "$b" ]
@@ -16,8 +30,15 @@ do
             if [ $? -ne 0 ]; then
                 exit $?
             fi
+        elif [ -n "$arbitraryCommand" ]
+        then
+            if [ -n "$reverseFileNames" ]; then
+                $arbitraryCommand "$c/$a" "$b/$a"
+            else
+                $arbitraryCommand "$b/$a" "$c/$a"
+            fi
         else
-            echo "$counter. will copy $c/$a to $b/$a"
+            echo "$counter. $a"
         fi
         a=
         b=
@@ -30,9 +51,50 @@ do
         fi
     fi
 done < "$DIRNAME/CHANGE-SUMMARY.txt"
-echo
 if [ -n "$confirmed" ]; then
+    echo
     echo "Done"
-else
-    echo "NB: Add -f as first command line arg to actually copy and overwrite"
+elif [ -z "$arbitraryCommand" ]; then
+    echo
+    echo "Usage: $CMDNAME [-f | [--rev] <arbitraryCommandWithArgs>]"
+    echo "where"
+    echo "    -f                              use to actually copy and overwrite"
+    echo ""
+    echo '    <arbitraryCommandWithArgs>      use to specify a command which will be called iteratively'
+    echo "                                    with each source file and its corresponding generated file" 
+    echo "                                    appended to its args. Can prepend command with --rev to"
+    echo "                                    rather have generated file appended before corresponding"
+    echo "                                    source file. Exit value of command invocation in each iteration"
+    echo "                                    is NOT checked."
+    echo ""
+    echo "NB: If no arguments are specified, only relative paths of source files are printed and then "
+    echo "this help message is printed afterwards."
+    echo ""
+    echo "EXAMPLES"
+    echo ""
+    echo "1. Use"
+    echo "      $CMDNAME -f"
+    echo '   to overwrite each source file with its corresponding generated file to achieve synchronization'
+    echo '   goal of Code Augmentor.'
+    echo ""
+    echo '2. Can use'
+    echo "      $CMDNAME echo"
+    echo '   to view full paths of each changed source file and its corresponding generated file on a line.'
+    echo ""
+    echo '3. Can use'
+    echo "      $CMDNAME diff"
+    echo '   to use built-in diff program on Unix/Linux to view file differences'
+    echo "" 
+    echo '4. Can use'
+    echo "      $CMDNAME git diff --no-index"
+    echo "   to view file differences with Git and get coloured console output."
+    echo "" 
+    echo '5. Can use'
+    echo "      $CMDNAME git --no-pager diff --no-index > diff.txt"
+    echo '   to save file differences with Git.'
+    echo ""
+    echo '6. Can use'
+    echo "      $CMDNAME --rev cp -f -v"
+    echo "   to achieve similar effect as $CMDNAME -f (but without error checking)"
+    echo ""
 fi
--- code-augmentor-core/src/main/resources/com/aaronicsubstances/code/augmentor/core/tasks/windows-copy-batch-file.bat
+++ code-augmentor-core/src/main/resources/com/aaronicsubstances/code/augmentor/core/tasks/windows-copy-batch-file.bat
@@ -1,8 +1,33 @@
 @echo off
 setlocal enabledelayedexpansion
-if "%1" == "-f" set confirmed=1
+
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
+
+set CMDNAME=%~n0
+
+if "%1" == "--rev" (
+    set reverseFileNames=1
+    shift
+)
+
+set args=%1
+shift
+:start
+if [%1] == [] goto done
+set args=%args% %1
+shift
+goto start
+
+:done
+rem can use %args% now
+
+set arbitraryCommand=%args%
+if "%arbitraryCommand%" == "-f" (
+    set confirmed=1
+    set arbitraryCommand=
+)
+
 for /F "usebackq tokens=*" %%A in ("%DIRNAME%\CHANGE-SUMMARY.txt") do (
     if defined b (
         set c=%%A
@@ -13,8 +38,14 @@ for /F "usebackq tokens=*" %%A in ("%DIRNAME%\CHANGE-SUMMARY.txt") do (
             if !ERRORLEVEL! neq 0 (
                 exit /B !ERRORLEVEL!
             )
+        ) else if defined arbitraryCommand (
+            if defined reverseFileNames (
+                !arbitraryCommand! !c!\!a! !b!\!a!
+            ) else (
+                !arbitraryCommand! !b!\!a! !c!\!a!
+            )
         ) else (
-            echo !COUNTER!. will copy !c!\!a! to !b!\!a!
+            echo !COUNTER!. !a!
         )
         set a=
         set b=
@@ -24,10 +55,54 @@ for /F "usebackq tokens=*" %%A in ("%DIRNAME%\CHANGE-SUMMARY.txt") do (
         set a=%%A
     )
 )
-echo.
 if defined confirmed (
+    echo.
     echo Done
+) else if defined arbitraryCommand (
+    rem Done
 ) else (
-    echo NB: Add -f as first command line arg to actually copy and overwrite
+    echo.
+    echo Usage: %CMDNAME% [-f ^| [--rev] ^<arbitraryCommandWithArgs^>]
+    echo where
+    echo     -f                              use to actually copy and overwrite
+    echo.
+    echo     ^<arbitraryCommandWithArgs^>      use to specify a command which will be called iteratively 
+    echo                                     with each source file and its corresponding generated file 
+    echo                                     appended to its args. Can prepend command with --rev to 
+    echo                                     rather have generated file appended before corresponding
+    echo                                     source file. Exit value of command invocation in each iteration
+    echo                                     is NOT checked.
+    echo.
+    echo NB: If no arguments are specified, only relative paths of source files are printed and then 
+    echo this help message is printed afterwards.
+    echo.
+    echo EXAMPLES
+    echo.
+    echo 1. Use
+    echo       %CMDNAME% -f
+    echo    to overwrite each source file with its corresponding generated file to achieve synchronization
+    echo    goal of Code Augmentor.
+    echo.
+    echo 2. Can use
+    echo       %CMDNAME% echo
+    echo    to view full paths of each changed source file and its corresponding generated file on a line.
+    echo.
+    echo 3. Can use
+    echo       %CMDNAME% fc /n /t
+    echo    to use built-in File Compare program on Windows to view file differences ^(it assumes files are in 
+    echo    ASCII encoding^)
+    echo. 
+    echo 4. Can use
+    echo       %CMDNAME% git diff --no-index
+    echo    to view file differences with Git and get coloured console output.
+    echo. 
+    echo 5. Can use
+    echo       %CMDNAME% git --no-pager diff --no-index ^> diff.txt
+    echo    to save file differences with Git.
+    echo.
+    echo 6. Can use
+    echo       %CMDNAME% --rev copy /B /Y
+    echo    to achieve similar effect as %CMDNAME% -f ^(but without error checking^)
+    echo.
 )
 endlocal
