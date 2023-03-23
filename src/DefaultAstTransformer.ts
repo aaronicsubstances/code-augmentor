25b81f3b8ea781db4225ff59822897ceb8e11e18
Aaron Baffour-Awuah <aaronbaffourawuah@gmail.com>
2023-03-23 10:23:38 +0000

    Fixed bug with generated code processing during default AST transformations.

--- src/DefaultAstTransformer.ts
+++ src/DefaultAstTransformer.ts
@@ -139,44 +139,38 @@ export default class DefaultAstTransformer {
 
     static _consolidateAugCodeArgs(args: any[]) {
         const blocks = new Array<any>();
-        let lastBlockIsJson: boolean | null = null;
-        const block = new Array<any>();
-        let startIdx = 0;
-        while (startIdx < args.length) {
-            if (args[startIdx] === null) {
-                lastBlockIsJson = null;
-                startIdx++;
+        let i = 0;
+        while (i < args.length) {
+            if (args[i] === null) {
+                i++;
                 continue;
             }
-            // clear block items
-            block.length = 0;
-            let j;
-            for ( j = startIdx; j < args.length; j+=3) {
-                const lineContent = args[j];
+            const blockBuilder = new Array<any>();
+            let lastBlockIsJson = false;
+            for (; i < args.length; i+=3) {
+                const lineContent = args[i];
                 if (lineContent === null) {
                     break;
                 }
-                const jsonify = args[j + 2] as boolean;
-                if (lastBlockIsJson === null) {
+                const jsonify = args[i + 2] as boolean;
+                if (blockBuilder.length) {
+                    if (lastBlockIsJson !== jsonify) {
+                        break;
+                    }
+                    // add prev line sep 
+                    blockBuilder.push(args[i - 3 + 1]);
+                }
+                else {
                     lastBlockIsJson = jsonify;
                 }
-                else if (lastBlockIsJson !== jsonify) {
-                    break;
-                }
-                if (j > startIdx) {
-                    // add prev line sep 
-                    block.push(args[j - 3 + 1]);
-                }
-                block.push(lineContent);
+                blockBuilder.push(lineContent);
             }
             if (lastBlockIsJson) {
-                blocks.push(JSON.parse(block.join("")));
+                blocks.push(JSON.parse(blockBuilder.join("")));
             }
             else {
-                blocks.push(block.join(""));
+                blocks.push(blockBuilder.join(""));
             }
-            lastBlockIsJson = !lastBlockIsJson;
-            startIdx = j;
         }
 
         return blocks;
@@ -184,52 +178,66 @@ export default class DefaultAstTransformer {
 
     applyGeneratedCodes(augCode: AugmentingCodeDescriptor,
             genCodes: GeneratedCode[]) {
-        const augCodeNode = augCode.parentNode.children[augCode.idxInParentNode] as 
-            (NestedBlockAstNode | DecoratedLineAstNode);
-        const genCodeTransforms = new Array<GeneratedCodeSectionTransform | null>();
         const genCodeSections = this.extractGenCodeSections(augCode);
 
-        // begin by processing all but the last gen code.
-        let genCodeSectionReduction = 0;
-        if (augCode.nestedBlockUsed) {
-            const lastGenCodeSection = getLast(genCodeSections);
-            if (lastGenCodeSection && lastGenCodeSection.parentNode === augCode.parentNode) {
-                genCodeSectionReduction++;
-            }
-        }
+        const augCodeNode = augCode.parentNode.children[augCode.idxInParentNode] as 
+            (NestedBlockAstNode | DecoratedLineAstNode);
         let defaultIndent = augCodeNode.indent;
         let defaultLineSep = augCodeNode.lineSep;
-        this._addGenCodeTransforms(
-            defaultIndent, defaultLineSep,
-            genCodes.slice(0, -1),
-            genCodeSections.slice(0, genCodeSections.length - genCodeSectionReduction),
-            genCodeTransforms);
 
-        // now process last gen code.
-        if (genCodeSectionReduction) {
+        const augCodeTransforms = new Array<DefaultAstTransformSpec>();
+        if (augCode.nestedBlockUsed) {
+            // processing all but the last gen code first,
+            // before processing the last gen code.
+            let genCodeSectionReduction = 0;
+            const lastGenCodeSection = getLast(genCodeSections);
+            if (lastGenCodeSection && lastGenCodeSection.parentNode === augCode.parentNode) {
+                genCodeSectionReduction = 1;
+            }
+            this._addAugCodeTransforms(
+                augCode, false,
+                defaultIndent, defaultLineSep,
+                genCodes.slice(0, -1),
+                genCodeSections.slice(0, genCodeSections.length - genCodeSectionReduction),
+                augCodeTransforms);
+
+            // change defaults for last gen code processing.
             defaultIndent = (augCodeNode as NestedBlockAstNode).endIndent;
             defaultLineSep = (augCodeNode as NestedBlockAstNode).endLineSep;
+            this._addAugCodeTransforms(
+                augCode, true,
+                defaultIndent, defaultLineSep,
+                genCodes.slice(-1),
+                genCodeSections.slice(genCodeSections.length - genCodeSectionReduction),
+                augCodeTransforms);
         }
-        this._addGenCodeTransforms(defaultIndent, defaultLineSep,
-            genCodes.slice(-1),
-            genCodeSections.slice(genCodeSections.length - genCodeSectionReduction),
-            genCodeTransforms);
-        const augCodeTransforms = this.computeAugCodeTransforms(augCode,
-            genCodeTransforms, genCodeSections);
+        else {
+            // ignore all but the last gen code.
+            this._addAugCodeTransforms(
+                augCode, true,
+                defaultIndent, defaultLineSep,
+                genCodes.slice(-1),
+                genCodeSections,
+                augCodeTransforms);
+        }
+
         DefaultAstTransformer.performTransformations(augCodeTransforms);
     }
 
-    _addGenCodeTransforms(
+    _addAugCodeTransforms(
+            augCode: AugmentingCodeDescriptor,
+            transformParentOfAugCodeNode: boolean,
             defaultIndent: string,
             defaultLineSep: string,
             genCodes: GeneratedCode[],
             genCodeSections: GeneratedCodeDescriptor[],
-            dest: Array<GeneratedCodeSectionTransform | null>) {
+            dest: Array<DefaultAstTransformSpec>) {
+        const genCodeTransforms = new Array<GeneratedCodeSectionTransform | null>;
         let minCodeSectionsToDealWith = Math.min(genCodes.length, genCodeSections.length);
         for (let i = 0; i < minCodeSectionsToDealWith; i++) {
             const genCode = genCodes[i];
             if (!genCode) {
-                dest.push(null);
+                genCodeTransforms.push(null);
                 continue;
             }
             const transform: GeneratedCodeSectionTransform = {
@@ -251,12 +259,12 @@ export default class DefaultAstTransformer {
                     genCodeSection, genCodeIndent, genCodeLineSep);
                 transform.node = node;
             }
-            dest.push(transform);
+            genCodeTransforms.push(transform);
         }
         for (let i = minCodeSectionsToDealWith; i < genCodes.length; i++) {
             const genCode = genCodes[i];
             if (!genCode) {
-                dest.push(null);
+                genCodeTransforms.push(null);
                 continue;
             }
             const transform: GeneratedCodeSectionTransform = {
@@ -272,8 +280,11 @@ export default class DefaultAstTransformer {
                     null, defaultIndent, defaultLineSep);
                 transform.node = node;
             }
-            dest.push(transform);
+            genCodeTransforms.push(transform);
         }
+
+        DefaultAstTransformer.computeAugCodeTransforms(augCode, transformParentOfAugCodeNode,
+            genCodeSections, genCodeTransforms, dest);
     }
 
     extractGenCodeSections(augCode: AugmentingCodeDescriptor) {
@@ -476,59 +487,21 @@ export default class DefaultAstTransformer {
     /**
      * Determines the list of transformations that will keep an aug code section in
      * sync with some specified list of gen code sections.
-     * <p>
-     * By accepting any gen code sections, this method is tolerating wrong
-     * gen code sections. To make sense of them, this method simply syncs
-     * nested gen code sections with all but the last gen code,
-     * and syncs the last gen code with the first non-nested gen code section.
-     * <p>It must be mentioned that this behaviour works perfectly for the correct
-     * case of gen code sections properly aligned with their aug code sections.
-     * @param genCodes
-     * @param augCode
-     * @param genCodeSections
-     * @returns
+     * @param augCode 
+     * @param transformParentOfAugCodeNode 
+     * @param genCodeSections 
+     * @param genCodes 
+     * @param dest 
      */
-    computeAugCodeTransforms(
+    static computeAugCodeTransforms(
             augCode: AugmentingCodeDescriptor,
-            genCodes: Array<GeneratedCodeSectionTransform | null>,
-            genCodeSections: GeneratedCodeDescriptor[] | null) {
-        const augCodeNode = augCode.parentNode.children[augCode.idxInParentNode];
-        if (!genCodeSections) {
-            genCodeSections = this.extractGenCodeSections(augCode);
-        }
-        const transforms = new Array<DefaultAstTransformSpec>();
-        const nestedGenCodeSections = new Array<GeneratedCodeDescriptor>();
-        const nonNestedGenCodeSections = new Array<GeneratedCodeDescriptor>();
-        for (const genCodeSection of genCodeSections) {
-            if (genCodeSection.parentNode === augCodeNode) {
-                // don't mind if another nested aug code is the correct owner.
-                nestedGenCodeSections.push(genCodeSection);
-            }
-            else {
-                // don't mind if gen code is unrelated to aug code.
-                nonNestedGenCodeSections.push(genCodeSection);
-            }
-        }
-        if (augCode.nestedBlockUsed) {
-            this._addAugCodeTransforms(genCodes.slice(0, -1), augCode,
-                nestedGenCodeSections, augCodeNode as NestedBlockAstNode, transforms);
-        }
-        else {
-            // not needed for correct gen code sections.
-            this._addAugCodeTransforms([], augCode,
-                nestedGenCodeSections, augCode.parentNode, transforms);
-        }
-        // ignore all but the last gen code for non nested sections.
-        this._addAugCodeTransforms(genCodes.slice(-1), augCode,
-            nonNestedGenCodeSections, augCode.parentNode, transforms);
-        return transforms;
-    }
-
-    _addAugCodeTransforms(genCodes: Array<GeneratedCodeSectionTransform | null>,
-            augCode: AugmentingCodeDescriptor,
+            transformParentOfAugCodeNode: boolean,
             genCodeSections: GeneratedCodeDescriptor[],
-            targetNode: SourceCodeAst | NestedBlockAstNode,
-            transformSpecs: DefaultAstTransformSpec[]) {
+            genCodes: Array<GeneratedCodeSectionTransform | null>,
+            dest: DefaultAstTransformSpec[]) {
+        const targetNode = transformParentOfAugCodeNode ? augCode.parentNode :
+            augCode.parentNode.children[augCode.idxInParentNode] as (SourceCodeAst | NestedBlockAstNode);
+
         const minCodeSectionsToDealWith = Math.min(genCodes.length, genCodeSections.length);
 
         // deal with gen code sections which have to be updated.
@@ -545,7 +518,7 @@ export default class DefaultAstTransformer {
                     replacementChild: null,
                     performDeletion: true
                 };
-                transformSpecs.push(transformSpec);
+                dest.push(transformSpec);
             }
             else if (!genCode.ignore) {
                 // update.
@@ -556,7 +529,7 @@ export default class DefaultAstTransformer {
                     replacementChild: genCode.node,
                     performDeletion: false
                 };
-                transformSpecs.push(transformSpec);
+                dest.push(transformSpec);
             }
         }
 
@@ -579,7 +552,7 @@ export default class DefaultAstTransformer {
                     replacementChild: null,
                     performDeletion: true
                 };
-                transformSpecs.push(transformSpec);
+                dest.push(transformSpec);
             }
         }
 
@@ -616,7 +589,7 @@ export default class DefaultAstTransformer {
                 replacementChild: null,
                 performDeletion: false
             };
-            transformSpecs.push(transformSpec);
+            dest.push(transformSpec);
         }
     }
 
